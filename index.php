a73318fb8565205d09e03c1ad2212db5bb366e19
maekawatatsuya1217 <84844702+maekawatatsuya1217@users.noreply.github.com>
2021-12-09 16:41:03 +0900

    トップページを作成

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>php chat app</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+<body>
+  <div class="header">
+    <div class="header-left">Chat app</div>
+    <div class="header-right">
+      <ul>
+        <li>カテゴリー1</li>
+        <li>カテゴリー2</li>
+        <li class="selected">カテゴリー3</li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="main">
+    <div class="contact-form">
+      <div class="form-title">new chat</div>
+      <form action="sent.php" method="post">
+      </form>
+    </div>
+  </div>
+  
+  <div class="footer">
+    <div class="footer-left">
+      <ul>
+        <li>カテゴリー1</li>
+        <li>カテゴリー2</li>
+        <li>カテゴリー3</li>
+      </ul>
+    </div>
+    <div class="like-box">
+      <iframe src="https://www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2FProgate%2F742679992421539&amp;show_faces=false" scrolling="no" frameborder="0" style="border:none; overflow:hidden; height:300px;" allowTransparency="true"></iframe>
+    </div>
+  </div>
+</body>
+</html>
